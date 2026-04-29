29ee2583372258c4acb3b90302d2dba329fb3adc
claravitoria <limaclarinha17@gmail.com>
2026-04-29 10:58:33 -0300

    Soma de Números

---
+++ ex02/script.js
@@ -0,0 +1,7 @@
+let soma = 0;
+
+for (let i = 1; i <= 10; i++){
+    let numero = Number(prompt("Digite um número - Faça esse processo 10 vezes"));
+    soma = soma + numero;
+}
+alert ("a soma total é: " + soma);
